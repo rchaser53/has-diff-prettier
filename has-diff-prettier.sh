888b0ac0d2be1ce86a39b27f3d62f767a1344080
rChaser53 <tayoshizawa29@gmail.com>
2019-01-30 10:08:07 +0900

    change the position to define COUNT

--- has-diff-prettier.sh
+++ has-diff-prettier.sh
@@ -1,10 +1,10 @@
-COUNT=`git diff`
-
 if [ -n "$FORMATTER_COMMAND" ]; then
   $FORMATTER_COMMAND
 else
   npm run prettier
 fi
+
+COUNT=`git diff`
 
 if [ ${#COUNT} -gt 0 ]; then
   echo "should prettier before commit"
